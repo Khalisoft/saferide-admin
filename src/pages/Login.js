64ebba67803f09c5c38df3f83f2cd840a8a60525
Muhammad Khalil <pkhalisoft@gmail.com>
2023-07-19 02:08:34 +0100

    more config

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
-import ImageLight from "../assets/img/hantsi-logo.png";
-import ImageDark from "../assets/img/hantsi-logo.png";
+import ImageLight from "../assets/img/logo-lockup-light.png";
+import ImageDark from "../assets/img/logo-lockup-dark.png";
 import { GithubIcon, TwitterIcon } from "../icons";
 import { Label, Input, Button } from "@windmill/react-ui";
 import { AuthContext } from "./../context/AuthContext";
@@ -59,20 +59,20 @@ function Login() {
 	return (
 		<div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
 			<div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-				<div className="flex flex-col overflow-y-auto md:flex-row">
+				<div className="flex flex-col justify-center items-center overflow-y-auto md:flex-row">
 					{/* <Label className="absolute text-center">
 						Election Monitoring System
 					</Label> */}
-					<div className="h-32 md:h-auto md:w-1/2">
+					<div className=" md:w-1/2">
 						<img
 							aria-hidden="true"
-							className="object-cover w-full h-full dark:hidden"
+							className="w-full dark:hidden"
 							src={ImageLight}
 							alt="Office"
 						/>
 						<img
 							aria-hidden="true"
-							className="hidden object-cover w-full h-full dark:block"
+							className="hidden w-full dark:block"
 							src={ImageDark}
 							alt="Office"
 						/>
@@ -103,7 +103,7 @@ function Login() {
 							</Label>
 
 							<Button
-								className="mt-4"
+								className="mt-4 bg-primary"
 								block
 								// tag={Link} to="/app"
 								onClick={() => signinFn()}
